3a0849d52a224a9e375c47e1e80d5cc2f1888716
Ori Komemie <orikomemi90@gmail.com>
2024-05-11 15:18:50 +0300

    winVar4

--- file1.js
+++ file1.js
@@ -1,7 +1,8 @@
 // file 1
-const var1 = "from mac-01";
 const winVar = "winVar";
 const winVar2 = "winVar2";
 const winVar3 = "winVar3";
+const winVar4 = "winVar4";
+
 const macVar = "macVar";
 const macVar2 = "macVar2";
